b5b841e74aa31df4fcfd1783a65178af1682ecac
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-02-05 18:12:03 -0800

    color is adding to state and rendering correctly

--- src/screens/ColorsScreen.js
+++ src/screens/ColorsScreen.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColor] = useState('');
   return <View>
-    <Button title='Click for more colors'/>
-    <Text style={styles.colors}>Color Screen</Text>
+    <Button title='Click for more colors' onPress={() => {
+      setColor([...colors,randomRGB()])
+      console.log('colors in state', colors)
+      }}/>
+    
+    <FlatList 
+      data={colors} 
+      keyExtractor={(item) => item}
+      renderItem={({item}) => {
+      return <View style={{height: 100, width: 100, backgroundColor: item}}></View>
+    }}/>
   </View>;
 };
 
+const randomRGB = () => {
+  let red = Math.floor(Math.random() * 256)
+  let blue = Math.floor(Math.random() * 256)
+  let green = Math.floor(Math.random() * 256)
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
 const styles = StyleSheet.create({
-  colors:{
-    width: 100,
-    height: 100,
-    backgroundColor: 'red'
-  }
+
 })
 export default ColorScreen;
