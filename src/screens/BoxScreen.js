11a561bc59b80e25b7c2e07c71da7f8c5b9b8b34
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-02-09 13:09:34 -0800

    finished layout examples

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,14 +3,49 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return(
-    <View>
-      <Text>The Box screen Component</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle1}>1</Text>
+      <Text style={styles.textStyle2}>2</Text>
+      <Text style={styles.textStyle3}>3</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+  viewStyle : {
+    borderWidth: 1,
+    borderColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 100,
+  },
+  textStyle1: {
+    color: 'blue',
+    margin: 5,
+    borderWidth: 2,
+    borderColor: 'green',
+    padding: 2,
+    backgroundColor: 'green',
+    alignSelf: 'flex-start',
+  },
+  textStyle2: {
+    color: 'blue',
+    margin: 5,
+    borderWidth: 2,
+    borderColor: 'green',
+    padding: 2,
+    backgroundColor: 'blue',
+    alignSelf: 'flex-end',
+  },
+  textStyle3: {
+    color: 'blue',
+    margin: 5,
+    borderWidth: 2,
+    borderColor: 'green',
+    padding: 2,
+    backgroundColor: 'red',
+    alignSelf: 'flex-start',
+  },
 })
 
 export default BoxScreen;
